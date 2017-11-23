58084cd25e02d3183e8abc8caec9224207c752a0
huipeng <huipengly@gmail.com>
2017-11-24 01:37:25 +0800

    add 变异函数

---
+++ variation.m
@@ -0,0 +1,16 @@
+function [ newchro ] = variation( chro, pm )
+%VARIATION 变异函数
+%   在[1,length-1]范围内产生两个变异位，变异位之间左右翻转
+
+length = size(chro,2);%列数
+
+if pm > rand()
+   r1=round(rand*(length-2))+1;  %在[1,length-1]范围内随机产生一个变异位  
+   r2=round(rand*(length-2))+1;  
+   chr1=min(r1,r2);  
+   chr2=max(r1,r2);  
+   x=chro(chr1+1:chr2);  
+   newchro(chr1+1:chr2)=fliplr(x);%变异位之间左右翻转
+end
+
+end
