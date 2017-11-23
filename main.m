ac700f716f1111bdd0a78c8edcd5e48cc498952b
huipeng <huipengly@gmail.com>
2017-11-24 01:55:22 +0800

    遗传算法主体调用

--- main.m
+++ main.m
@@ -1,6 +1,32 @@
-lb = [ones(1,20) 0 0 0 0 0];
-ub = [20*ones(1,20) 20 20 20 100 100];
-% Aeq = [[zeros(1,23) 1 0];[zeros(1,24) 1]];
-% beq = [[zeros(1,23) 29 0];[zeros(1,24) 59]];
-IntCon = 1:25;
-x=ga(@fitness, 25, [], [], [], [], lb, ub, @nonlcon, IntCon);
+load p.mat
+load d.mat
+
+inn=30; %初始种群大小
+gnmax=500;  %最大代数
+pc=0.8; %交叉概率
+pm=0.08; %变异概率
+
+%产生初始种群
+Population=zeros(inn,20);
+for i=1:inn
+    Population(i,:)=randperm(20);
+end
+
+[f,p]=Fitness(Population,dislist);
+
+gn=1;
+
+ChildPopulation = zeros(inn, 20);
+while gn <= gnmax
+    for i = 1:2:inn
+        %交叉
+       parent = select(p);
+       %scro=cro(s,seln,pc);  %交叉操作
+       %交叉结果变异
+       ChildPopulation(i,:) = variation(children(1), pm);
+       ChildPopulation(i+1,:) = variation(children(2), pm);
+    end
+    Population = ChildPopulation;
+    [f,p]=Fitness(Population,dislist);
+    %TODO:fitness函数，修改，输入染色体，输出不变
+end
