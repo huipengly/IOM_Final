0a36e897af26b436aaa89f108005a5f41617abc3
huipeng <huipengly@gmail.com>
2017-11-24 00:31:07 +0800

    自己写GA，添加个体值函数

--- fitness.m
+++
@@ -1,7 +0,0 @@
-function [ out ] = fitness(x)
-%FITNESS 此处显示有关此函数的摘要
-%   此处显示详细说明
-global p;
-p(1)
-out = sum(p(x(x(21)+x(22)+1:20))) + 59;
-end
---
+++ value.m
@@ -0,0 +1,43 @@
+function [ day, a, b ] = value(x)
+%FITNESS 计算个体值
+%   此处显示详细说明
+global p;
+global d;
+
+% q(i)代表第i个工件的加工顺序
+Q=[x(1:20);1:20];
+Q1=sortrows(Q',1)';
+q=Q1(2,:);
+
+%寻找第一次维护前的生产个数a
+for i = 1:20
+    aaa = sum(p(x(1:i)));
+    if aaa > 29
+       a = i - 1; 
+       break;
+    end
+end
+
+%寻找第一次维护到第二次维护期间的生产个数b
+for i = a:20
+    if sum(p(x(a+1:i))) > (59 - 29)
+       b = i - 1 - a; 
+       break;
+    end
+end
+
+%判断是否满足交货期，不满足则day直接返回-1
+for i = 20:-1:1
+   actday = -d(i) + (sum(p(x(1:q(i)))))*(q(i)<=a) + ...
+                (30 + sum(p(x(a+1 : q(i))))) * ((a < q(i)) & (q(i) <= a + b)) + ...
+                (60 + sum(p(x(a+b+1 : q(i))))) * (a + b < q(i));
+    if actday > 0
+        day = -1;
+        return;
+    end
+end
+
+%返回总日期
+day = sum(p(x(a+b+1 : 20))) + 60;
+
+end
