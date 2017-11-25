1de8311fbdf45431acef719984ed924ad0f37379
huipeng <huipengly@gmail.com>
2017-11-25 16:24:16 +0800

    解决种群进化中，求取适值函数bug

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ load p.mat
 load d.mat
 
 inn=30; %初始种群大小
-gnmax=500;  %最大代数
+gnmax=20;  %最大代数
 pc=0.8; %交叉概率
 pm=0.08; %变异概率
 
@@ -18,20 +18,30 @@ end
 
 gn=1;
 
-ymean=zeros(gnmax,1);  
-ymin=zeros(gnmax,1);  
-xmin=zeros(inn,20);  
+ymean=zeros(gnmax,1);
+ymin=zeros(gnmax,1);
+cmin=zeros(inn,20);
+ym=1e10;%全局最优解
+cm=[];%全局最优解序列
 ChildPopulation = zeros(inn, 20);
-while gn <= gnmax
+while gn < gnmax
    [vmin,nmin]=min(v);
    if ~isnan(vmin)
        aaa = 1;
    end
-   ymean(gn)=mean(v(~isnan(v)));
-   ymin(gn)=vmin;
-   x=Population(nmin,:);
-   xmin(gn,:)=x;
-    for i = 1:2:inn
+%    if ~isnan(v)
+%        aaa = 1;
+%    end
+   ymean(gn,:) = mean(v(~isnan(v)));
+   ymin(gn, :)=vmin;
+   cmin(gn,:)=Population(nmin,:);
+%    if vmin < ym
+%        %保留历史最优解
+%       ym = vmin;
+%       cm = Population(nmin,:);
+%    end
+   %TODO:保留最优的前2个解
+   for i = 1:2:inn
         %交叉
        parent = select(pr);
        children = PMX(Population(parent, :), pc);  %交叉操作
@@ -44,9 +54,15 @@ while gn <= gnmax
 %        ChildPopulation(i+1,:) = variation(children(2, :), pm);
     end
     Population = ChildPopulation;
-    [pr, f]=Fitness(Population);
+    [pr, f, v]=Fitness(Population);
     gn = gn + 1
 end
-x
-value(x)
+[vmin,nmin]=min(v);
+ymean(gn, :)=mean(v(~isnan(v)));
+ymin(gn, :)=vmin;
+cmin(gn, :)=Population(nmin,:);
+[Min,nMin] = min(ymin)
+cMin = cmin(nMin,:)
+value(cMin)
 toc
+plot(ymin)
